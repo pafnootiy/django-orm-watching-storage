c129b5244adc78c32f6f3f1328e8e479a927036f
pafnootiy <ksander-86@mail.ru>
2022-06-02 20:13:39 +0300

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,30 +1,21 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
-from datacenter.models import Duration
-from datacenter.models import Format
-from datacenter.models import VisitLong
-
 from django.shortcuts import render
 
 
 def storage_information_view(request):
     non_closed_visits = []
-    all_visits = Visit.objects.all()
-    for visit in all_visits:
-        if visit.leaved_at:
-            duration = Duration(visit).get_time_in_storage(visit)
-            format_time = Format(duration).format_duration(duration)
-            continue
-        else:
-            person_in_storage = {
-                'who_entered': visit.passcard.owner_name,
-                'entered_at': visit.entered_at,
-                'duration': format_time
+    all_visits_in_storage = Visit.objects.filter(leaved_at=None)
+    for visit in all_visits_in_storage:
+        duration = Visit(visit).get_time_in_storage(visit)
+        format_time = Visit(duration).format_duration(duration)
+        person_in_storage = {
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': format_time
+        }
 
-            }
-            non_closed_visits.append(person_in_storage)
-
+        non_closed_visits.append(person_in_storage)
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
