647586f4de98567b83de6057a0ac65ebc90f8dc7
pafnootiy <ksander-86@mail.ru>
2022-06-05 22:55:17 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,33 +4,26 @@ from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[9]
-    person_passcard = Visit.objects.filter(passcard=passcard)
+    passcards = Passcard.objects.all()
+    passcode = Passcard.objects.get(passcode=passcode)
     serialized_visits = []
-    for visit in person_passcard:
-        if visit.leaved_at:
-            duration = Visit(visit).get_duration(visit)
-            format_time = Visit(duration).format_duration(duration)
-            visit_long = Visit(visit, 58, duration).is_visit_long(visit, 58, duration)
-            person_in = {
-                'entered_at': visit.entered_at,
-                'duration': format_time,
-                'is_strange': visit_long
-            }
-            serialized_visits.append(person_in)
-        else:
-            duration = Visit(visit).get_time_in_storage(visit)
-            format_time = Visit(duration).format_duration(duration)
-            visit_long = Visit(visit, 58, duration).is_visit_long(visit, 58, duration)
-            person_in = {
-                'entered_at': visit.entered_at,
-                'duration': format_time,
-                'is_strange': visit_long
-            }
-            serialized_visits.append(person_in)
+    for passcard in passcards:
+        person_visits = Visit.objects.filter(passcard=passcode)
+
+
+    for visit in person_visits:
+        duration = visit.get_duration()
+        format_time = visit.format_duration(duration)
+        visit_long = visit.is_visit_long(58, duration)
+        this_passcard_visits = {
+            'entered_at': visit.entered_at,
+            'duration': format_time,
+            'is_strange': visit_long
+        }
+        serialized_visits.append(this_passcard_visits)
 
     context = {
-        'passcard': passcard,
+        'passcard': passcode,
         'this_passcard_visits': serialized_visits
     }
     return render(request, 'passcard_info.html', context)
