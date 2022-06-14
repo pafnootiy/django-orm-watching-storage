c20be1d3d1f41b6807ec7e14ce7da40a06e7dadd
pafnootiy <ksander-86@mail.ru>
2022-06-15 00:29:48 +0300

    import argparse for personal settings
    This reverts commit 0268d0fbb22b298a96e3437728b917ecc8b3fc3f.

--- project/settings.py
+++ project/settings.py
@@ -1,30 +1,41 @@
 import os
 from dotenv import load_dotenv
 from environs import Env
+import argparse
 
 env = Env()
 env.read_env()
 load_dotenv()
-user = os.getenv("USER")
-password = os.getenv("PASSWORD")
-secret_key = os.getenv("SECRET_KEY")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': user,
-        'PASSWORD': password,
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
     }
 }
 
+parser = argparse.ArgumentParser(
+    description='Описание что делает программа'
+)
+parser.add_argument('HOST', help='Ваш хост')
+parser.add_argument('--PORT', help='Номер порта', type=int)
+parser.add_argument('--NAME', help='Ваше имя')
+parser.add_argument('--USER', help='Ваш логин')
+parser.add_argument('--PASSWORD', help='Ваш пароль')
+args = parser.parse_args()
+print("HOST", args.HOST)
+print('PORT', args.PORT)
+print('NAME', args.NAME)
+print('USER', args.USER)
+print('PASSWORD', args.PASSWORD)
+
 INSTALLED_APPS = ['datacenter']
-
-SECRET_KEY = secret_key
-
-DEBUG = env.bool("DEBUG")
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG")
 ROOT_URLCONF = 'project.urls'
 ALLOWED_HOSTS = ['*']
 
