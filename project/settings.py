3df953f1fae8ffadf21d2c620f6eecb20db3c610
pafnootiy <ksander-86@mail.ru>
2022-06-16 16:57:16 +0300

    took config out of code

--- project/settings.py
+++ project/settings.py
@@ -1,10 +1,6 @@
 import os
 from dotenv import load_dotenv
-from environs import Env
-import argparse
 
-env = Env()
-env.read_env()
 load_dotenv()
 
 DATABASES = {
@@ -18,26 +14,12 @@ DATABASES = {
     }
 }
 
-parser = argparse.ArgumentParser(
-    description='Описание что делает программа'
-)
-parser.add_argument('HOST', help='Ваш хост')
-parser.add_argument('--PORT', help='Номер порта', type=int)
-parser.add_argument('--NAME', help='Ваше имя')
-parser.add_argument('--USER', help='Ваш логин')
-parser.add_argument('--PASSWORD', help='Ваш пароль')
-args = parser.parse_args()
-print("HOST", args.HOST)
-print('PORT', args.PORT)
-print('NAME', args.NAME)
-print('USER', args.USER)
-print('PASSWORD', args.PASSWORD)
-
 INSTALLED_APPS = ['datacenter']
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 ROOT_URLCONF = 'project.urls'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
