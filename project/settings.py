e2f11af698828ae5ae84e476b78164217b6de4cb
pafnootiy <ksander-86@mail.ru>
2022-06-21 19:24:27 +0300

    add indents

--- project/settings.py
+++ project/settings.py
@@ -1,16 +1,19 @@
 import os
+
 from dotenv import load_dotenv
 from environs import Env
+
 load_dotenv()
 env = Env()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
-        'NAME': os.getenv("NAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
     }
 }
 
@@ -19,7 +22,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 ROOT_URLCONF = 'project.urls'
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
